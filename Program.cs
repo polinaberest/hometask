fbffda5f9982de9739c07e51d34f1e9693f2adc3
Polina Berest <pamapolya@gmail.com>
2022-01-28 18:56:40 +0200

    try

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace delegates_hometask
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("VNVNVNVVN");
         }
     }
 }
